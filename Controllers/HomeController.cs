69c5b449dc1bed85a23b95ab4060ec7c48e680e8
user <zabakacper03@gamil.com>
2024-12-08 16:54:13 +0100

    working app

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KacperZaba.DAL;
+using KacperZaba.ViewModels;
+
+namespace KacperZaba.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UniversityContext db = new UniversityContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            IQueryable<EnrollmentDateGroups> data =
+                from student in db.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroups()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(data.ToList());
+        }
+
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ DAL/UniversityContext.cs
@@ -0,0 +1,24 @@
+﻿using KacperZaba.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace KacperZaba.DAL
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext() : base("UniversityContext")
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
